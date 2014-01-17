8e87d9276906863bb0973a1291013527f313f44f
Cristian Prieto <me@cprieto.com>
2014-01-17 17:37:47 +1100

    Added constants for plugin and plugin configuration file

---
+++ common/src/PluginConstants.java
@@ -0,0 +1,5 @@
+public interface PluginConstants {
+    String RUNNER_TYPE = "monotouch.runner";
+    String RUNNER_NAME = "Monotouch Build Runner";
+    String RUNNER_DESCRIPTION = "Build Xamarin's Monotouch projects";
+}
